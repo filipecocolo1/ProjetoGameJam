f3b5c027d0497150c9b6de8fe6ab6a0de222ecbf
DonRossini <36886780+DonRossini@users.noreply.github.com>
2020-08-29 19:57:16 -0300

    Ceu
    Ceu ajustado e colocado nas posições certas!

---
+++ tESTEGameJam2d/Assets/Animetion/_SunSetScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _SunSetScript : MonoBehaviour
+{
+
+    public GameObject DaySky;
+    public GameObject MidDaySky;
+    public GameObject LateDaySky;
+    public GameObject NightSky;
+
+    public GameObject _CameraFog;
+
+    public float _tempo;
+
+    private void Update()
+    {
+
+        _tempo += Time.deltaTime;
+
+        //if (_tempo > 6) 
+        //{
+        //    LateDaySky.SetActive(false);
+        //}
+        //else if (_tempo > 4) 
+        //{
+        //    MidDaySky.SetActive(false);
+        //}
+        //else if (_tempo > 2) 
+        //{
+        //    DaySky.SetActive(false);
+        //}
+    }
+
+}
