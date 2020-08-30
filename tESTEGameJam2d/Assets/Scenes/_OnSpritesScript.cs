e0aebdbe22f6a61f5473d493698632893eaab570
filipecocolo1 <53791311+filipecocolo1@users.noreply.github.com>
2020-08-30 02:46:09 -0300

    Comitando att

---
+++ tESTEGameJam2d/Assets/Scenes/_OnSpritesScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _OnSpritesScript : MonoBehaviour
+{
+
+    public GameObject _Sprite_Oceano_Frente;
+    public GameObject _Sprite_Barco_Frente;
+    public GameObject _Sprite_Oceano_BG;
+
+    private void OnTriggerEnter2D(Collider2D oth)
+    {        
+        if (oth.CompareTag("player"))
+        {
+            _Sprite_Oceano_Frente.SetActive(true);
+            _Sprite_Barco_Frente.SetActive(true);
+            _Sprite_Oceano_BG.SetActive(false);
+        }
+    }
+}
